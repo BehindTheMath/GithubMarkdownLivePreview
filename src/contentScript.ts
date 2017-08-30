4b3667e146191367efdef3758927968ae8360a5b
Behind The Math <info@behindthemath.io>
2017-08-30 01:22:40 -0400

    Add HTML sanitizing

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -89,6 +89,47 @@ namespace GithubMarkdownLivePreview {
             timeoutId = null;
         }
 
-        previewContentElement.innerHTML = textAreaValue ? converter.makeHtml(textAreaValue) : "Nothing to preview";
+        if (textAreaValue) {
+            const html: string = converter.makeHtml(textAreaValue);
+
+            const sanitizeHtmlOptions: sanitize.IOptions = {
+                allowedTags: ["h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "br", "b", "i", "strong", "em", "a", "pre",
+                    "code", "img", "tt", "div", "ins", "del", "sup", "sub", "p", "ol", "ul", "table", "thead", "tbody",
+                    "tfoot", "blockquote", "dl", "dt", "dd", "kbd", "q", "samp", "var", "hr", "ruby", "rt", "rp", "li",
+                    "tr", "td", "th", "s", "strike", "summary", "details"],
+                allowedAttributes: {
+                    a: ["href"],
+                    img: ["src", "longdesc"],
+                    div: ["itemscope", "itemtype"],
+                    blockquote: ["cite"],
+                    del: ["cite"],
+                    ins: ["cite"],
+                    q: ["cite"],
+                    "*": [
+                        "abbr", "accept", "accept-charset", "accesskey", "action", "align", "alt", "axis", "border",
+                        "cellpadding", "cellspacing", "char", "charoff", "charset", "checked", "clear", "cols", "colspan",
+                        "color", "compact", "coords", "datetime", "dir", "disabled", "enctype", "for", "frame", "headers",
+                        "height", "hreflang", "hspace", "ismap", "label", "lang", "maxlength", "media", "method", "multiple",
+                        "name", "nohref", "noshade", "nowrap", "open", "prompt", "readonly", "rel", "rev", "rows", "rowspan",
+                        "rules", "scope", "selected", "shape", "size", "span", "start", "summary", "tabindex", "target",
+                        "title", "type", "usemap", "valign", "value", "vspace", "width", "itemprop"]
+                },
+                allowedSchemes: ["http", "https", "mailto", "github-windows", "github-mac"],
+                allowedSchemesByTag: {
+                    a: ["http", "https", "mailto", "github-windows", "github-mac"],
+                    blockquote: ["http", "https"],
+                    del: ["http", "https"],
+                    ins: ["http", "https"],
+                    q: ["http", "https"],
+                    img: ["http", "https"],
+                },
+                allowProtocolRelative: true
+            };
+            
+            const sanitizedHTML: string = sanitizeHtml(html, sanitizeHtmlOptions);
+            previewContentElement.innerHTML = sanitizedHTML;
+        } else {
+            previewContentElement.textContent = "Nothing to preview";
+        }
     }
 }
